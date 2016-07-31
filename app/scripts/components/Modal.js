8d658f5f1786bf1ea4395082977d92cf71786e08
lindsay cooper <lindsaysaracooper@gmail.com>
2016-07-31 17:58:02 -0500

    adding points to the schore board

--- app/scripts/components/Modal.js
+++ app/scripts/components/Modal.js
@@ -3,19 +3,34 @@ import { Link, hashHistory } from 'react-router';
 import store from "../store";
 
 const Modal = React.createClass({
+  getInitialState: function (){
+    return{
+      userRight: null,
+    }
+  },
 
   modalClickHandler:function(e){
     hashHistory.push('/');
 },
   answerClickHandler: function(e){
     e.preventDefault();
-    let answer = this.refs.answer.value;
-    if (this.state.question.answer === answer){
-      console.log("KICKING ASS TAKING NAMES");
+    let scoreboard;
+    let initialValue=store.session.get('score')
+    let points;
+    let answerValue = this.refs.answer.value;
 
-  }
+    if (this.state.question.answer === answerValue){
+      this.setState({userRight:true});
+      points = Number(initialValue += answerValue)
+      store.session.set('score', points)
+      console.log('YAS QUEEN');
 
-  },
+    } else {
+       this.setState({userRight: false});
+       points = Number(initialValue -= answerValue)
+       store.session.set('score', points)
+     }
+   },
 
 
    openModal () { this.setState({open: true}); },
